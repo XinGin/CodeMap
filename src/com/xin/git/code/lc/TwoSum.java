cdd5480136f6c9ea4564e82d38c4f0b90233e006
XinGin <peixin.li2@gmail.com>
2013-12-24 16:19:02 +0800

    0.0.2

--- src/com/xin/git/code/lc/TwoSum.java
+++ src/com/xin/git/code/lc/TwoSum.java
@@ -6,28 +6,28 @@ public class TwoSum {
     
 	public int[] twoSum(int[] numbers, int target) {
 
-			//One
-			Map<Integer, Integer> numIndex = new HashMap<Integer, Integer>();
-			int[] res = new int[2];
-			for(int i = 0; i < numbers.length; i++){
-				if(numIndex.containsKey(target-numbers[i]))
-					return new int[]{numIndex.get(target - numbers[i])+1,  i+1};
-				numIndex.put(numbers[i], i);
-			}
+		//One
+		Map<Integer, Integer> numIndex = new HashMap<Integer, Integer>();
+		int[] res = new int[2];
+		for(int i = 0; i < numbers.length; i++){
+			if(numIndex.containsKey(target-numbers[i]))
+				return new int[]{numIndex.get(target - numbers[i])+1,  i+1};
+			numIndex.put(numbers[i], i);
+		}
 	        
-			return res;
+		return res;
 /*	
-			//Two
-			int[] res = new int[2];
-			for(int i = 0; i < numbers.length; i++)
-    			for(int j = i+1; j < numbers.length; j++)
-					if( numbers[i]+numbers[j] == target){
-						res[0] = i+1;
-						res[1] = j+1;
-						break;
-					}
+		//Two
+		int[] res = new int[2];
+		for(int i = 0; i < numbers.length; i++)
+    		for(int j = i+1; j < numbers.length; j++)
+				if( numbers[i]+numbers[j] == target){
+					res[0] = i+1;
+					res[1] = j+1;
+					break;
+				}
 
-			return res;
+		return res;
 */
 	}
 
