203146c8f87a10c6765d120b12db1063829157b4
XinGin <peixin.li2@gmail.com>
2013-12-25 13:29:00 +0800

    0.0.1

--- src/com/xin/git/code/lc/AbsDistinct.java
+++ src/com/xin/git/code/lc/AbsDistinct.java
@@ -8,13 +8,13 @@ class AbsDistinct {
     
 	public int absDistinct(int[] A) {
         
-        Map<Long, Integer> davHash = new HashMap<Long, Integer>();
+		Map<Long, Integer> davHash = new HashMap<Long, Integer>();
         
-        for(int i=0; i < A.length; i++)
-            if(!davHash.containsKey((long)Math.abs(A[i])))
-                davHash.put((long)Math.abs(A[i]), null);
+		for(int i=0; i < A.length; i++)
+			if(!davHash.containsKey((long)Math.abs(A[i])))
+				davHash.put((long)Math.abs(A[i]), null);
         
-        return davHash.size();
+		return davHash.size();
               
     }
 }
