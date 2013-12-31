7eda512a880d12901f2d16648aec2113539ac2cc
XinGin <peixin.li2@gmail.com>
2013-12-31 14:46:37 +0800

    0.0.1

---
+++ src/com/xin/git/code/lc/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package com.xin.git.code.lc;
+
+public class PalindromeNumber {
+	public boolean isPalindrome(int x){
+		
+		if(x < 0)
+			return false;
+		if(x < 10)
+			return true;
+		
+		int div = 10;
+		
+		while(x / div >= 10){
+			div *= 10;
+		}
+		
+		int left = 0;
+		int right = 0;
+		while(div >= 10){
+			left = x / div;
+			right = x % 10;
+			if(left != right)
+				return false;
+			x = (x % div) / 10;
+			div /= 100;
+		}
+				
+		return true;
+	}
+}
--- src/com/xin/git/code/lc/TestLab.java
+++ src/com/xin/git/code/lc/TestLab.java
@@ -19,17 +19,19 @@ public class TestLab {
 		int[] res = new int[2];
 		res = twoSum.twoSum(numbers, 4);
 		System.out.println("Index1:"+res[0]+","+"Index2:"+res[1]);
-		//END TwoSum
-		
+		//2.SingleNumber
 		SingleNumber singleNumber = new SingleNumber();
 		int[] numbers2 = {1,1,2,3,4,5,4,3,2};
 		int res2 = singleNumber.singleNumber(numbers2);
 		System.out.println(res2);
-		
+		//3.MultiplyStrings
 		MultiplyStrings mS = new MultiplyStrings();
 		String num1 = "111111";
 		String num2 = "111111";
 		System.out.println(mS.multiply(num1, num2));
+		//4.PalindromeNumber
+		PalindromeNumber pN = new PalindromeNumber();
+		System.out.println(pN.isPalindrome(1011));
 	}
 
 }
