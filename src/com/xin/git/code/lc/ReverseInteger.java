a6f260c168904a437f786b3914a4655b99320f00
XinGin <peixin.li2@gmail.com>
2014-01-02 17:25:08 +0800

    0.0.1

--- src/com/xin/git/code/lc/ReverseInteger.java
+++ src/com/xin/git/code/lc/ReverseInteger.java
@@ -8,18 +8,22 @@ public class ReverseInteger {
       sign = -1;
       x *= -1;
     }
+    
     long res = this.reverse((long)x);
     if((sign * res) > Integer.MAX_VALUE || (sign * res) < Integer.MIN_VALUE)
       throw new RuntimeException("Reversed Integer is out of bounds!");
+    
     return (int)(sign * res);
   }
   
   private long reverse(long x){
+    
     long res = 0;
     while(x != 0){
       res = (res * 10) + (x % 10);
       x /= 10;
     }
+    
     return res;
   }
 }
